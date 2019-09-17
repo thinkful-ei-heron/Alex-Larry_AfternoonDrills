00e311475b58c2238750c5b46325e6cd05fe6c7b
Alex Bannow <rbannal@gmail.com>
2019-09-17 12:14:06 -0400

    added code for functions as arguments 1

---
+++ drills.js
@@ -0,0 +1,11 @@
+function repeat(fn, n) {
+  for(i=0; i < n; i++) {
+    fn();
+  }
+}
+function hello() {
+  console.log('Hello World');
+}
+function goodbye() {
+  console.log('Goodbye World');
+}
