0df99324aba11449a2944476ab603b4bf3c8c1ca
Alex Bannow <rbannal@gmail.com>
2019-09-17 13:28:33 -0400

    added functions as return values code

--- drills.js
+++ drills.js
@@ -1,4 +1,4 @@
-function repeat(fn, n) {
+/*function repeat(fn, n) {
   for(i=0; i < n; i++) {
     fn();
   }
@@ -20,4 +20,31 @@ function filter(myNames, fn) {
     }
   })
   return newNames;
+}*/
+
+function hazardWarningCreator (typeOfWarning) {
+  let warningCounter = 0;
+  return function(location) {
+    warningCounter++;
+    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+  }
 }
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const snowWarning = hazardWarningCreator('Lots of Snow');
+const tsunamiWarning = hazardWarningCreator('Big Huge Waves');
+
+rocksWarning('Main St and Pacific Ave');
+rocksWarning('Centinela Ave and Olympic Blvd');
+rocksWarning('14th Ave and Crescent');
+
+tsunamiWarning('Right behind you');
+tsunamiWarning('On your left');
+tsunamiWarning('A few feet in front of you');
+
+snowWarning('Cold Lane and Snow Place');
+snowWarning('Forest Blvd and Mountainview');
+snowWarning('All over the place');
+
+
